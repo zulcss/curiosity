3722e05886cb0965b1ac21da015e12e4ddf7cf4f
Chuck Short <charles.short@ericsson.com>
2017-07-31 11:20:33 -0400

    Point to 0.0.0.0
    Signed-off-by: Chuck Short <charles.short@ericsson.com>

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 var globalConf = {
-    defaultServer : "http://localhost:9200", 
-    confServer : "http://localhost:9200",
+    defaultServer : "http://0.0.0.0:9200", 
+    confServer : "http://0.0.0.0:9200",
     confIndex : "curiosity-config",
     defaultConfDocumentType : "conf-doc",
     defaultContextDocumentType : "context-doc",
